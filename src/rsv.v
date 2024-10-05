3995613e27d23c8a5d9bc705bdcfbd0b6a2d0d2c
Pastilhas <joao.campos1221@gmail.com>
2024-10-05 21:49:14 +0000

    format

--- src/rsv.v
+++ src/rsv.v
@@ -1,12 +1,10 @@
 module rsv
 
-const (
-	eov_flag  = 0xFF
-	null_flag = 0xFE
-	eor_flag  = 0xFD
-	empty_val = ''
-	null_val  = none
-)
+const eov_flag = 0xFF
+const null_flag = 0xFE
+const eor_flag = 0xFD
+const empty_val = ''
+const null_val = none
 
 // unvalidated encode
 pub fn encode(table [][]?string) []u8 {
@@ -17,13 +15,13 @@ pub fn encode(table [][]?string) []u8 {
 			if str := val {
 				bytes << str.bytes()
 			} else {
-				bytes << rsv.null_flag
+				bytes << null_flag
 			}
 
-			bytes << rsv.eov_flag
+			bytes << eov_flag
 		}
 
-		bytes << rsv.eor_flag
+		bytes << eor_flag
 	}
 
 	return bytes
@@ -36,11 +34,11 @@ pub fn decode(bytes []u8) [][]?string {
 	mut start := 0
 
 	for i := 0; i < bytes.len; i++ {
-		if bytes[i] == rsv.eov_flag {
+		if bytes[i] == eov_flag {
 			if i == start {
-				row << rsv.empty_val
-			} else if bytes[start] == rsv.null_flag {
-				row << rsv.null_val
+				row << empty_val
+			} else if bytes[start] == null_flag {
+				row << null_val
 			} else {
 				row << bytes[start..i].bytestr()
 			}
@@ -49,7 +47,7 @@ pub fn decode(bytes []u8) [][]?string {
 			continue
 		}
 
-		if bytes[i] == rsv.eor_flag {
+		if bytes[i] == eor_flag {
 			table << row
 			row = []?string{}
 			start += 1
